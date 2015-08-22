9e5e003cea6e3efbff5f701f3103173f74d574e9
gustiazis <gooazs@gmail.com>
2015-08-22 17:55:29 +0700

    Left Behind From Last Commit

--- scripts/bootstrap-extended.js
+++ scripts/bootstrap-extended.js
@@ -278,7 +278,7 @@
 		if (that.hasClass('plus')) {
 
 			var clonedItem = baseMode.clone().insertBefore(baseMode);
-			clonedItem.removeClass('base-mode').addClass('edit-mode').animateCss('slideInDown');
+			clonedItem.removeClass('base-mode').addClass('edit-mode');
 			clonedItem.find('input[type=text], textarea, select').filter(':visible:first').focus();
 			actsMode.find('a').css('display', 'none').end().find('a:last-child').css('display', 'inline-block');
 
